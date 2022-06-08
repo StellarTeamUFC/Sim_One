83780f8a4483f7b9142485fe60d9477fc97cb7a3
Sarah Barbosa <106551299+SarahBarbosa@users.noreply.github.com>
2022-06-08 14:32:47 -0300

    Produto final

--- sim_one.py
+++ sim_one.py
@@ -10,7 +10,7 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from stochastic.processes.noise import FractionalGaussianNoise # pip install stochastic
+from stochastic.processes.noise import FractionalGaussianNoise   # pip install stochastic
 
 # Funções criadas
 
@@ -38,7 +38,7 @@ Gday = Gday.value
 Rstar = 1 * R_sun               # Raio da estrela em unidades solares
 Rplanet = 0.1 * R_sun           # Fração de raios solares. Raio da Terra = 0.00915 Rsun
 depth = (Rplanet/Rstar)**2      # Profundidade de trânsito
-Porb = 150                      # Período orbital em dias
+Porb = 15                       # Período orbital em dias
 ai = 90                         # Ângulo de inclinação da estrela (entre 0 e 90 graus)
 cadence_kepler = 0.020833       # Cadência em dias (Missão Kepler)
 cadence_plato = 0.0002895       # Cadência em dias (Missão Plato)
@@ -96,7 +96,7 @@ x1 = (1 - depth) * np.ones(ntF)
 x2 = np.ones(interTransit)
 
 rep = 10
-pulse = np.concatenate([x2, rampdow,x1,rampup,x2])
+pulse = np.concatenate([rampdow,x1,rampup,x2])
 pulse_train = np.matlib.repmat(pulse, 1, rep)
 
 PT = pulse_train[0]
@@ -122,7 +122,7 @@ nPT = PT + noise
 
 fig, ax = plt.subplots(figsize = (20,10))
 
-plt.scatter(time,nPT, c = 'black')
+plt.scatter(time,nPT, c = 'black', s = 1)
 plt.xlabel('tempo (em dias baricêntricos Julianos)')
 plt.ylabel('Fluxo normalizado')
 plt.title('Trânsito planetário com ruído')
@@ -131,15 +131,15 @@ plt.title('Trânsito planetário com ruído')
 
 # Parâmetros de entrada
 
-# Paper base: BONOMO, Aldo S.; LANZA, Antonino F. Starspot activity and rotation of the planet-hosting star Kepler-17. 
-#Astronomy & Astrophysics, v. 547, p. A37, 2012.
+# Paper base: BONOMO, Aldo S.; LANZA, Antonino F. Starspot activity and rotation 
+# of the planet-hosting star Kepler-17. Astronomy & Astrophysics, v. 547, p. A37, 2012.
 
 gamma1 = 0.3985             # gamma 1 e 2 são parâmetros do escurecimento do limbo
 gamma2 = 0.2586  
            
 ap = 1 - gamma1 - gamma2    # ap, bp e cp são coeficientes que nós dá a magnitude
-bp = gamma1 + 2*gamma2      # bolométrica da fotosfera como uma função do ângulo
-cp = -gamma2                # do limbo adotado como uma lei quadrátca do escurecimento
+bp = gamma1 + 2 * gamma2      # bolométrica da fotosfera como uma função do ângulo
+cp = - gamma2                # do limbo adotado como uma lei quadrátca do escurecimento
                             # do limbo
 
 cs = 0.850                  # coeficiente que especifica o contraste do spot bolométrico
@@ -147,38 +147,38 @@ cf0 = 1.115
 Q = 0.5                     # é uma função da fase do ciclo solar
 As = 0.01                   # é a área do spot da região ativa em unidades de ...
 
-ii = ai * pi/180            # ângulo de inclinação em reseito da linha de visada
+ii = ai * pi / 180            # ângulo de inclinação em reseito da linha de visada
 theta = 0.17 * pi           # é a posição da região ativa na estrela 
              
 Peq = 15                    # em dias
 AmpliP = 0.2                # alpa = DeltaPer/Per
 
-P2 = Peq * (1 - AmpliP * (sin(theta)) ** 2) ** (-1)
+P2 = Peq * (1 - AmpliP * (sin(theta) ** 2)) ** (-1)
 lambda_ = 2 * pi / 5
 
 
 # Spot 1
 
-omega = 2 * pi / Peq        # velocidade angular da estrela
+omega_1 = 2 * pi / Peq        # velocidade angular da estrela
 
 c = (ap + 2 * bp / 3 + cp / 2) ** (-1)
 
 # Ângulo do limbo:
-mii = cos(ii) * cos(theta) + sin(ii) * sin(theta) * cos(lambda_ + omega * np.linspace(0, time[-1], len(time))) 
+mii_1 = cos(ii) * cos(theta) + sin(ii) * sin(theta) * cos(lambda_ + omega_1 * np.linspace(0, time[-1], len(time))) 
 
-cf = cf0 * (1 - mii)        # Intensidade do contraste da facúla
+cf = cf0 * (1 - mii_1)        # Intensidade do contraste da facúla
 
-s1 = 1 + As * (ap + bp * mii + cp * mii ** 2) * c * (Q * cf - cs) * mii
+s1 = 1 + As * (ap + bp * mii_1 + cp * mii_1 ** 2) * c * (Q * cf - cs) * mii_1
 
 # Spot 2
 
-omega = 2 * pi / P2
+omega_2 = 2 * pi / P2
 
-mii = cos(ii) * cos(theta) + sin(ii) * sin(theta) * cos(lambda_ + omega * np.linspace(0, time[-1], len(time))) 
+mii_2 = cos(ii) * cos(theta) + sin(ii) * sin(theta) * cos(lambda_ + omega_2 * np.linspace(0, time[-1], len(time))) 
 
-cf = cf0 * (1 - mii)
+cf = cf0 * (1 - mii_2)
 
-s2 = 1 + As * (ap + bp * mii + cp * mii ** 2) * c * (Q * cf - cs) * mii
+s2 = 1 + As * (ap + bp * mii_2 + cp * mii_2 ** 2) * c * (Q * cf - cs) * mii_2
 
 spots = (s1 + s2) - np.mean(s1 + s2)
 
@@ -187,6 +187,6 @@ spots = (s1 + s2) - np.mean(s1 + s2)
 wts = spots + nPT
 
 fig, ax = plt.subplots(figsize = (20,10))
-plt.scatter(time, wts, c = 'black')
+plt.scatter(time, wts, c = 'black', s = 1)
 plt.xlabel('tempo (dias baricêntricos Julianos)')
 plt.ylabel('Fluxo normalizado')
